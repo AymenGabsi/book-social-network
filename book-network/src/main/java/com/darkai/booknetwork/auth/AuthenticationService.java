2e98051769b32fd0c08da5eb9358f97e6eafab58
Aymen Gabsi <aymengabsi20@gmail.com>
2024-05-27 20:56:18 +0200

    [BookSocialNetwork] Implement The Register Method

--- book-network/src/main/java/com/darkai/booknetwork/auth/AuthenticationService.java
+++ book-network/src/main/java/com/darkai/booknetwork/auth/AuthenticationService.java
@@ -1,9 +1,78 @@
 package com.darkai.booknetwork.auth;
 
+import com.darkai.booknetwork.role.RoleRepository;
+import com.darkai.booknetwork.user.Token;
+import com.darkai.booknetwork.user.TokenRepository;
+import com.darkai.booknetwork.user.User;
+import com.darkai.booknetwork.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
     public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                // TODO Better Exception Handling !
+                .orElseThrow(() -> new IllegalStateException("Role USER was not initialized !"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // Generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+
+//        emailService.sendEmail(
+//                user.getEmail(),
+//                user.getFullName(),
+//                EmailTemplateName.ACTIVATE_ACCOUNT,
+//                activationUrl,
+//                newToken,
+//                "Account activation"
+//        );
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length()); // RandomIndex From 0 To 9
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
